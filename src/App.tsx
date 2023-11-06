a8eae768fd2c69e814c08f21d902e27b94c92378
p1geoner <pegeoner@mail.ru>
2023-11-06 10:21:37 +0300

    add component dice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <div></div>
+    <div>
+
+    </div>
   )
 }
 
---
+++ src/components/dice/ui/Dice.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+
+interface  IDiceProps {
+  id: string
+}
+
+const Dice:FC<IDiceProps>= () => {
+    const random
+    return()
+}
