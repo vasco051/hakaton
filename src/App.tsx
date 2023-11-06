f288d6327fe3e92c9fbcb5c941dd184a33aafe86
p1geoner <pegeoner@mail.ru>
2023-11-06 11:14:06 +0300

    add ui cubes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import BoardCell from './components/BoardCell/BoardCell';
-import { CellVariant } from './components/BoardCell/types';
+import {CellVariant} from './components/BoardCell/types';
+import {Dice} from "./components/dice/ui/Dice.tsx";
 
 function App() {
-  return (
-    <div>
-      <BoardCell variant={CellVariant.LEFT} item={{}}/>
-    </div>
-  );
+    const getDice = () => {
+
+    }
+    return (
+        <div>
+            <button onClick={getDice()}>dice</button>
+            <BoardCell variant={CellVariant.LEFT} item={{}}/>
+            <Dice userId={1}></Dice>
+        </div>
+    );
 }
 
 export default App;
--- src/components/dice/ui/Dice.tsx
+++ src/components/dice/ui/Dice.tsx
@@ -1,10 +1,28 @@
-import {FC} from "react";
-
+import {FC, useEffect, useState} from "react";
+import styles from "./styles.module.scss"
 interface  IDiceProps {
-  id: string
+  userId: number
 }
 
-const Dice:FC<IDiceProps>= () => {
-    const random
-    return()
+export const Dice:FC<IDiceProps>= () => {
+    const min =1;
+    const max =7;
+    const [random,setRandom] = useState<number[]>()
+    useEffect(()=>{
+        setRandom([Math.floor(Math.random() * (max - min) + min),Math.floor(Math.random() * (max - min) + min)])
+    },[])
+
+    return(
+        <div className={styles.wrapper}>
+            <div className={styles.cubesWrapper}>
+                <div className="cube">
+                    <div className="face front">Front</div>
+                    <div className="face back">Back</div>
+                </div>
+            </div>
+
+                {random}
+
+        </div>
+    )
 }
